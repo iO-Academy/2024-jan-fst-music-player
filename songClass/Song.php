9523ce799e887137a41f5b05d6dca3857bad27bf
Max Hamilton <maxeh987@gmail.com>
2024-04-08 15:09:13 +0100

    added song, artist, and album classes with getters

---
+++ songClass/Song.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace songClass;
+
+class Song
+{
+    private int $id;
+    private string $name;
+    private float $length;
+    private int $albumId;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getLength(): float
+    {
+        return $this->length;
+    }
+
+    public function getAlbumId(): int
+    {
+        return $this->albumId;
+    }
+}
