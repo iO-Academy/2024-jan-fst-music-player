667b716d331e55e1437cede2bff4f879e0e42429
richardjgabb <richardjgabb@.com>
2024-04-08 15:11:00 +0100

    composer install and hydrator classes

---
+++ src/ArtistHydrator/ArtistHydrator.php
@@ -0,0 +1,18 @@
+<?php
+
+class SongHydrator
+{
+    private static PDO $db;
+    public static function getArtists (): array
+    {
+        $query = \src\SongHydrator\SongHydrator::$db->prepare('SELECT `id`, `song_name`, `length`, `album_id` FROM `music`');
+        $query->execute();
+        $query->setFetchMode(PDO::FETCH_CLASS, Artist::class);
+        return $query->fetchAll;
+    }
+
+    public static function setDb(PDO $db): void
+    {
+        self::$db = $db;
+    }
+}
---
+++ src/SongHydrator/SongHydrator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace src\SongHydrator;
+use Song;
+
+class SongHydrator
+{
+    private static PDO $db;
+
+    public static function getSongsByAlbum($albumId): array
+    {
+        $query = SongHydrator::$db->prepare('SELECT `id`, `song_name`, `length`, `album_id` FROM `music` WHERE `album_id` = ?');
+        $query->execute([$albumId]);
+        $query->setFetchMode(PDO::FETCH_CLASS, Song::class);
+        return $query->fetchAll;
+    }
+
+    public static function setDb(PDO $db): void
+    {
+        self::$db = $db;
+    }
+}
