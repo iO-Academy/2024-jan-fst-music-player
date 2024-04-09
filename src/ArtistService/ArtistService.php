f7003bf0851ddd291f0fb587cd39ed8a13f1f7c9
richardjgabb <richardjgabb@.com>
2024-04-09 10:10:22 +0100

    parameters

--- src/ArtistService/ArtistService.php
+++ src/ArtistService/ArtistService.php
@@ -4,16 +4,17 @@ namespace CodersCanine\ArtistService;
 
 use CodersCanine\ArtistHydrator\ArtistHydrator;
 use CodersCanine\AlbumService\AlbumService;
+use CodersCanine\SongService\SongService;
 
 class ArtistService
 {
- public function createArtistProfile(AlbumService $albumService)
+ public function createArtistProfile(AlbumService $albumService, SongService $songService) : array
  {
      $artistArray = [];
      $artists = ArtistHydrator::getArtists();
      foreach ($artists as $artist)
      {
-       $artistToAdd = $albumService->createAlbumProfile($artist->getId());
+       $artistToAdd = $albumService->createAlbumProfile($artist->getId(), $songService);
         $artistArray += [$artistToAdd];
      }
      return $artistArray;
