46973b75cafeac811a45b812c96b12794ff5be97
Izayah Jordan <izayahjordan13@gmail.com>
2024-04-09 09:20:58 +0100

    Added Artist service and functionality

--- src/ArtistService/ArtistService.php
+++ src/ArtistService/ArtistService.php
@@ -4,5 +4,15 @@ namespace CodersCanine\ArtistService;
 
 class ArtistService
 {
-
+ public function createArtistProfile($albumHydrator, $artistId)
+ {
+     $artistArray = [];
+     $artists = ArtistHydrator::getArtists();
+     foreach ($artists as $artist)
+     {
+       $artistToAdd = AlbumService::createAlbumProfile($artist->getId());
+        $artistArray += [$artistToAdd];
+     }
+     return $artistArray;
+ }
 }
